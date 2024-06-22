e19e5fc08c4651c99a4d12847eca0d6b4b79327b
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-06-21 20:12:42 -0400

    update

---
+++ test.ts
@@ -0,0 +1,22 @@
+//Déclaration de variable avec types
+
+let nom: string = "Alice";
+let age: number = 30;
+const estMajeur: boolean = true;
+
+//types specifique
+let score: number; // Déclaration d'une variable sans initialisation
+score = 85; // Attribution d'une valeur par la suite
+
+// Types conplexes
+let listNombre: number[] = [1, 2, 3, 2, 1]; // tableau de nombre 
+
+let listString : string[] = ['hello', 'world'] //tableau de string 
+
+let tableauMixt: (number | string)[] = [1,'string',2,3,3, 'salut']
+
+//Tuples
+
+let coodonnes: [number, number]=[10,20]; // Tuple pour représenter des coordonnes
+
+
